88797455cf4fb8b48811dc980a16d12d4cf9d7a5
Emre Sener <emre.sener@trilogy.com>
2024-03-11 16:12:34 +0300

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# Variables passed as arguments to the script
 VPN_CLIENT_VERSION=$1
 VPN_USERNAME=$2
 VPN_PASSWORD=$3
@@ -12,6 +13,11 @@ DESIRED_MTU=${8:-1500}  # Default to 1500 if not set
 MINIMAL_MTU=1200  # Define a minimal MTU value
 decrement_value=$(( (DESIRED_MTU - MINIMAL_MTU) / MAX_SETUP_ATTEMPTS ))  # Calculate decrement value
 
+# Ensure apt package list is updated and required packages are installed
+sudo apt update
+sudo apt install -y wget curl debconf openssl
+
+# Function to set the MTU for the VPN interface
 setMTU() {
     if [ $DESIRED_MTU -eq 1500 ]; then
         echo "MTU is at the default value of 1500, no change needed."
@@ -23,9 +29,10 @@ setMTU() {
     fi
 }
 
+# Function to wait for the kvnet interface to become ready
 waitForKvnet() {
     start_time=$(date +%s)
-    while ! ip a show kvnet up | grep inet 2>/dev/null; do
+    while ! ip a show kvnet up | grep inet >/dev/null 2>&1; do
         current_time=$(date +%s)
         elapsed_time=$(($current_time - $start_time))
         echo "Waiting for kvnet to start... Elapsed time: $elapsed_time seconds"
@@ -40,6 +47,7 @@ waitForKvnet() {
     return 0
 }
 
+# Write VPN configuration parameters for the installation
 writeKerioConfigParam() {
   name=$1
   type=$2
@@ -47,6 +55,7 @@ writeKerioConfigParam() {
   echo "kerio-control-vpnclient-${VPN_CLIENT_VERSION}-linux-amd64 kerio-kvc/$name $type $value" >> kerio.params
 }
 
+# Configure VPN parameters
 writeKerioConfig() {
   writeKerioConfigParam server string central-kerio-vpn.devfactory.com
   writeKerioConfigParam username string $VPN_USERNAME
@@ -55,15 +64,12 @@ writeKerioConfig() {
   writeKerioConfigParam autodetect_accept boolean true
 }
 
+# Function to setup VPN client
 setupVpn() {
   writeKerioConfig
 
   if [ ! -f "/tmp/kerio-control-vpnclient-${VPN_CLIENT_VERSION}-linux-amd64.deb" ]; then
-    wget http://cdn.kerio.com/dwn/control/control-${VPN_CLIENT_VERSION}/kerio-control-vpnclient-${VPN_CLIENT_VERSION}-linux-amd64.deb
-  else
-    sudo /etc/init.d/kerio-kvc stop
-    echo "Kerio stop delay"
-    sleep 2
+    wget -O /tmp/kerio-control-vpnclient-${VPN_CLIENT_VERSION}-linux-amd64.deb "http://cdn.kerio.com/dwn/control/control-${VPN_CLIENT_VERSION}/kerio-control-vpnclient-${VPN_CLIENT_VERSION}-linux-amd64.deb"
   fi
 
   sudo debconf-set-selections kerio.params
@@ -78,51 +84,45 @@ setupVpn() {
   waitForKvnet
 }
 
-try_verify() {
-  curl -s --max-time $WAIT_KVN_TIMEOUT --cookie "TOTP_CONTROL=${VPN_AUTH_CODE}" http://10.212.255.245:4080//nonauth/totpVerify.cs
-  curl_retval=$?
-}
-
-sudo apt update && sudo apt install -y wget curl debconf openssl
-
+# Main loop to try setting up the VPN multiple times
 cd /tmp
-
 setup_attempts=1
 while [ $setup_attempts -le $MAX_SETUP_ATTEMPTS ]; do
   setupVpn && break
   echo "VPN setup failed. Attempt: $setup_attempts"
-  DESIRED_MTU=$((DESIRED_MTU - decrement_value))  # Decrement the MTU
+  DESIRED_MTU=$((DESIRED_MTU - decrement_value))
   if [ $DESIRED_MTU -lt $MINIMAL_MTU ]; then
-    DESIRED_MTU=$MINIMAL_MTU  # Ensure we don't go below the minimal MTU
+    DESIRED_MTU=$MINIMAL_MTU
   fi
   setup_attempts=$((setup_attempts + 1))
 done
 
 if [ $setup_attempts -gt $MAX_SETUP_ATTEMPTS ]; then
   echo "Error! VPN setup failed after $MAX_SETUP_ATTEMPTS attempts"
-  echo "Kerio VPN Client logs:"
-  echo "init.log:"
-  sudo cat /var/log/kerio-kvc/init.log
-  echo "error.log:"
-  sudo cat /var/log/kerio-kvc/error.log
-  echo "debug.log:"
-  sudo cat /var/log/kerio-kvc/debug.log
   exit 1
 fi
 
+# Function to verify the VPN connection
+try_verify() {
+  curl -s --max-time $WAIT_KVN_TIMEOUT --cookie "TOTP_CONTROL=${VPN_AUTH_CODE}" http://10.212.255.245:4080//nonauth/totpVerify.cs
+  curl_retval=$?
+}
+
+# Attempt to verify the VPN connection
 retry_count=1
 try_verify
-while [ $curl_retval -ne 0 -a $retry_count -lt $MAX_CURL_ATTEMPT ]; do
+while [ $curl_retval -ne 0 ] && [ $retry_count -lt $MAX_CURL_ATTEMPT ]; do
   echo "curl failed with ${curl_retval}. retrying (${retry_count})..."
   retry_count=$((retry_count + 1))
-  sleep 3
+  sleep 3  # Added delay between retries for better stability
   try_verify
 done
 
 if [ x"${curl_retval}" != x"0" ]; then
   echo "Error! curl failed ${retry_count} times"
   exit $curl_retval
+else
+  echo "VPN connection is established!"
 fi
 
-echo "VPN connection is established!"
 exit 0
